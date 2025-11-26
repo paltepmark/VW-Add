70b2bab5ee3631aad117c682021f8029107566b3
DIAMOND <119308851+paltepmark@users.noreply.github.com>
2025-11-26 20:25:49 +0800

    Update nightsintheforest.lua

--- nightsintheforest.lua
+++ nightsintheforest.lua
@@ -1,2 +1,2 @@
 -- COPY THIS AND PASTE IT IN YOUR EXECUTOR 👇
-loadstring(game:HttpGet("https://raw.githubusercontent.com/paltepmark/VW-Add/main/nightsintheforest2.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/paltepmark/VW-Add/refs/heads/main/nightsintheforest2.lua", true))()
